74e8e68bfc6c74f5ce9bc13b334ae727339f3b35
Caleb Trachte <cetrachte@hotmail.com>
2022-02-28 13:14:33 -0600

    add z-index calculations, perspective changes based on z axis values

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as core from "@theatre/core";
+import state from "../state.json";
 import studio from "@theatre/studio";
-
 // initialize the studio so the editing tools will show up on the screen
-studio.initialize();
+// parcel.js knows and assigns NODE_ENV, tree-shaking occurs automatically
+if (process.env.NODE_ENV === "development") {
+  studio.initialize();
+}
+
 // init project
-const demoProject = core.getProject("Demo Project");
+const demoProject = core.getProject("Demo Project", { state });
 // create scene, sheets are a collection of objects that can be animated together, compared to an Excel Sheet
 const sheet = demoProject.sheet("Scene");
 // create an object
@@ -24,26 +28,46 @@ div.style.cssText = `
     height: 100px;
     background: #EEE;
 `;
+div.oldHeight = 100;
+div.oldWidth = 100;
 
 setTimeout(() => {
+  // add div 'box' to screen
   document.body.appendChild(div);
   sheet.sequence.position = 0; // set sequence to a time-based position (0.5 seconds)
 }, 1000);
 
+let oldPosition = object.value.position;
 // hookup props to onChange
 object.onValuesChange((newValues) => {
   div.style.left = newValues.position.x + "px";
   div.style.top = newValues.position.y + "px";
-  div.style.zIndex = newValues.position.z + "px";
-  console.table(object.value);
+  let old = oldPosition.z;
+  let change = Math.abs(old) - Math.abs(newValues.position.z);
+  console.log(old, newValues.position.z, change)
+  //console.table("before", newValues.position.z, div.style.zIndex);
+
+  if (change > 0) {
+    div.style.height =
+        parseFloat(div.style.height) + parseFloat(Math.abs(change) * parseFloat(div.oldHeight)) + "px";
+    div.style.width =
+        parseFloat(div.style.width) +   parseFloat(Math.abs(change) * parseFloat(div.oldWidth)) + "px";
+  } else if ( change < 0) {
+    div.style.height =
+        parseFloat(div.style.height) -  parseFloat(Math.abs(change) * parseFloat(div.oldHeight)) + "px";
+    div.style.width =
+        parseFloat(div.style.width) -  parseFloat(Math.abs(change) * parseFloat(div.oldWidth)) + "px";
+  } else {
+    console.log("same z index onchange");
+  }
+  oldPosition = newValues.position;
 });
 
 // interaction with UI
-
 div.addEventListener("click", (e) => {
   sheet.sequence.play({
     rate: 4, // play at 4x speed
-    range: [0, 6], // how much of the sequence? 0-5 seconds
+    range: [0, 6], // how much of the sequence? 0-6 seconds
     iterationCount: 1, // how many times? 4 times.
   });
 });
