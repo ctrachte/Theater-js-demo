89946f569319165dfa160be5e68a01812f8c9ece
Caleb Trachte <cetrachte@hotmail.com>
2022-02-28 10:34:46 -0600

    add scene, props, position

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import studio from "@theatre/studio"
 
 // initialize the studio so the editing tools will show up on the screen
 studio.initialize()
+// init project
+const demoProject = core.getProject('Demo Project');
+// create scene, sheets are a collection of objects that can be animated together, compared to an Excel Sheet
+const sheet = demoProject.sheet("Scene")
+// create an object 
+const object = sheet.object('Box', {
+    position: {
+        x:0, // props are arbritrary until they are made significant by the user
+        y:0,
+        z:0
+    },
+
+})
+
